e33c6b78bbf899153b4dc5e607d8186d748543a0
cozykitten <116394977+cozykitten@users.noreply.github.com>
2024-03-14 00:57:09 +0100

    removing unnecessary stuff

--- events/discord/interactionCreate.js
+++ events/discord/interactionCreate.js
@@ -3,31 +3,7 @@ import { db, sync } from '../../src/dbManager.js';
 export default async (interaction) => {
 
 	if (!interaction.isChatInputCommand()) return;
-
-	//log command to lb for crashreport on next start
-	db.lastcall = {
-		userid: interaction.user.id,
-		command: interaction.commandName
-	}
-
-	if (interaction.options._subcommand) {
-		db.lastcall.subcommand = interaction.options._subcommand
-	}
-
-	if (interaction.options._hoistedOptions.length) {
-
-		db.lastcall.options = [];
-		for (const iterator of interaction.options._hoistedOptions) {
-			const option = {
-				name: iterator.name,
-				value: iterator.value
-			}
-			db.lastcall.options.push(option);
-		}
-	}
-	sync(db);
-
-	//execute command
+	
 	const command = interaction.client.commands.get(interaction.commandName);
 	if (command) command.execute(interaction);
 
